db2006ae470d1079018ab23e14f83046d65e9ab8
Morteza <mortezahossainzade11@gmail.com>
2024-01-24 14:28:30 +0430

    button

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,9 +1,25 @@
 import { Button } from "antd";
+import { useState } from "react";
 
 function All() {
+  const [loading, setloading] = useState(false);
+
+  const onButtonClick = (e) => {
+    console.log("Button clicked");
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 2000);
+  };
   return (
     <div>
-      <Button className="mx-2 my-2">My First button</Button>
+      <Button
+        className="mx-2 my-2 hover:bg-black hover:scale-105"
+        loading={loading}
+        onClick={onButtonClick}
+      >
+        My First button
+      </Button>
     </div>
   );
 }
