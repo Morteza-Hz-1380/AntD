2a15cf6a30f77cc06f7fc473f53e30f056673258
Morteza <mortezahossainzade11@gmail.com>
2024-01-25 10:26:04 +0430

    input

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,5 +1,6 @@
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import { useState } from "react";
+import { UserOutlined } from "@ant-design/icons";
 
 function All() {
   const [loading, setloading] = useState(false);
@@ -12,14 +13,14 @@ function All() {
     }, 2000);
   };
   return (
-    <div>
-      <Button
-        className="mx-2 my-2 hover:bg-black hover:scale-105"
-        loading={loading}
-        onClick={onButtonClick}
-      >
+    <div className="md:flex">
+      <Button className="mx-2 my-2 " loading={loading} onClick={onButtonClick}>
         My First button
       </Button>
+      <Input.Search className="max-w-lg mx-2 my-2" placeholder="Name" maxLength={12}
+      prefix={<UserOutlined />}
+      allowClear
+      ></Input.Search>
     </div>
   );
 }
