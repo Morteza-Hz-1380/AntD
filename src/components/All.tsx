981aadf432616447651489565b1101b42c75c43f
Morteza <mortezahossainzade11@gmail.com>
2024-01-25 14:48:11 +0430

    Table

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Select, Form } from "antd";
+import { Button, Input, Select, Form, Table } from "antd";
 import { useState } from "react";
 import { UserOutlined } from "@ant-design/icons";
 
@@ -6,7 +6,7 @@ function All() {
   const [loading, setloading] = useState(false);
   const fruits = ["Banana", "Mango", "Orange", "Cherry"];
 
-  const onButtonClick = (e) => {
+  const onButtonClick = (e: any) => {
     console.log("Button clicked");
     setloading(true);
     setTimeout(() => {
@@ -14,15 +14,63 @@ function All() {
     }, 2000);
   };
 
-  const onFinish = (e) => {
+  const onFinish = (e: any) => {
     console.log(e);
-    
-  }
+  };
+
+  const data = [
+    {
+      name: "Morteza",
+      age: 22,
+      address: "Address 1",
+      key: "1",
+    },
+    {
+      name: "Ali",
+      age: 23,
+      address: "Address 2",
+      key: "2",
+    },
+    {
+      name: "Yones",
+      age: 20,
+      address: "Address 3",
+      key: "3",
+    },
+    {
+      name: "Hojjat",
+      age: 21,
+      address: "Address 4",
+      key: "4",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "key",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "key",
+      sorter : (a: { age: number; },b: { age: number; })=>a.age-b.age
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "key",
+    },
+  ];
+
   return (
     <div className="flex flex-col mx-2 my-2 gap-5">
       <Button loading={loading} onClick={onButtonClick}>
         My First button
       </Button>
+
+
       <Input.Search
         className="max-w-lg "
         placeholder="Name"
@@ -30,6 +78,8 @@ function All() {
         prefix={<UserOutlined />}
         allowClear
       ></Input.Search>
+
+
 
       <h1>What are your favorite fruit</h1>
       <Select
@@ -45,6 +95,8 @@ function All() {
         ))}
       </Select>
 
+
+
       <Form onFinish={onFinish} className="max-w-lg ">
         <Form.Item label="User-Name" name="username">
           <Input placeholder="User Name" required></Input>
@@ -54,7 +106,7 @@ function All() {
         </Form.Item>
         <Form.Item>
           <Button
-            className="bg-blue-500 text-white"
+            className="bg-blue-800 text-white"
             block
             type="default"
             htmlType="submit"
@@ -63,6 +115,10 @@ function All() {
           </Button>
         </Form.Item>
       </Form>
+
+
+
+      <Table dataSource={data} columns={columns}></Table>
     </div>
   );
 }
