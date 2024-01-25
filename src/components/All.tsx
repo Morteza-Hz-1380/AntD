a8342f3b88cede009714c85bc0c5605b61c7ced3
Morteza <mortezahossainzade11@gmail.com>
2024-01-25 11:40:29 +0430

    Select

--- src/components/All.tsx
+++ src/components/All.tsx
@@ -1,9 +1,10 @@
-import { Button, Input } from "antd";
+import { Button, Input, Select } from "antd";
 import { useState } from "react";
 import { UserOutlined } from "@ant-design/icons";
 
 function All() {
   const [loading, setloading] = useState(false);
+  const fruits = ["Banana", "Mango", "Orange", "Cherry"];
 
   const onButtonClick = (e) => {
     console.log("Button clicked");
@@ -13,14 +14,28 @@ function All() {
     }, 2000);
   };
   return (
-    <div className="md:flex">
-      <Button className="mx-2 my-2 " loading={loading} onClick={onButtonClick}>
+    <div className="flex flex-col mx-2 my-2 gap-5">
+      <Button loading={loading} onClick={onButtonClick}>
         My First button
       </Button>
-      <Input.Search className="max-w-lg mx-2 my-2" placeholder="Name" maxLength={12}
-      prefix={<UserOutlined />}
-      allowClear
+      <Input.Search
+        className="max-w-lg "
+        placeholder="Name"
+        maxLength={12}
+        prefix={<UserOutlined />}
+        allowClear
       ></Input.Search>
+
+      <div>
+        <h1>What are your favorite fruit</h1>
+        <Select maxTagCount={2} allowClear mode="multiple" placeholder="Select fruit">
+          {fruits.map((fruit, index) => (
+            <Select.Option key={index} value={fruit}>
+              {fruit}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
     </div>
   );
 }
